6cc06d078f5434dc18937fab0d6b5118f9ef81c0
Saif-khann <saiffkhann.5@gmail.com>
2024-10-15 17:11:54 +0530

    change few comments in Day 1 project

--- Day1.py
+++ Day1.py
@@ -10,7 +10,6 @@
 # 2. Ask the user for the city that they grew up in and store it in a variable.
 # 3. Ask the user for the name of a pet and store it in a variable.
 # 4. Combine the name of their city and pet and show them their band name.
-
 # Note: Make sure the input cursor shows on a new line.
 
 # Example Output:
