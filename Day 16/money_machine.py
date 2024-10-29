4b2183ab15c5fb6f95e55fdc99f8e36388b54f47
Saif-khann <saiffkhann.5@gmail.com>
2024-10-29 23:32:45 +0530

    Create and push MoneyMachine class for handling transactions and payment processing.

---
+++ Day 16/money_machine.py
@@ -0,0 +1,42 @@
+class MoneyMachine:
+    """Handles money-related operations."""
+    
+    CURRENCY = "$"
+    COIN_VALUES = {
+        "quarters": 0.25,
+        "dimes": 0.10,
+        "nickles": 0.05,
+        "pennies": 0.01
+    }
+
+    def __init__(self):
+        # Initialize profit and money received
+        self.profit = 0
+        self.money_received = 0
+
+    def report(self):
+        """Prints the current profit."""
+        print(f"Money: {self.CURRENCY}{self.profit}")
+
+    def process_coins(self):
+        """Returns the total calculated from coins inserted."""
+        print("Please insert coins.")
+        # Collect the number of each type of coin from the user and calculate total
+        for coin in self.COIN_VALUES:
+            self.money_received += int(input(f"How many {coin}?: ")) * self.COIN_VALUES[coin]
+        return self.money_received
+
+    def make_payment(self, cost):
+        """Returns True when payment is accepted, or False if insufficient."""
+        self.process_coins()
+        # Check if received money is enough to cover the cost
+        if self.money_received >= cost:
+            change = round(self.money_received - cost, 2)
+            print(f"Here is {self.CURRENCY}{change} in change.")
+            self.profit += cost
+            self.money_received = 0
+            return True
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+            self.money_received = 0
+            return False
